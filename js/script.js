49f4349200cfbab2bf125881d174cd11cc5b4e7d
Andrew59-boop <millerfaabi@gmail.com>
2020-02-29 23:32:26 +0300

    all changes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Contact(fname, address) {
+    this.fName =name;
+    this.addresses = [];
+  }
+  function Address(street, city, county) {
+      this.street = street;
+      this.city = city;
+      this.county = county;
+    }
+  $(document).ready(function(){
+  $(".A").hover(function(){
+    $(".large").slideToggle()
+  });
+  $(".B").hover(function(){
+    $(".medium").slideToggle()
+  });
+  $(".C").hover(function(){
+    $(".small").slideToggle()
+  });
+  
+  })
